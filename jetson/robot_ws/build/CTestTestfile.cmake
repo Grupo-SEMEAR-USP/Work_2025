0b36f048396985f154c7506e64696a42f82acf34
MatheusPaivaa <matheuzu11@gmail.com>
2025-04-02 15:06:42 -0300

    creating ros repo

---
+++ jetson/robot_ws/build/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: /home/math/Work_2025/jetson/robot_ws/src
+# Build directory: /home/math/Work_2025/jetson/robot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_msgs")
+subdirs("robot_bringup")
+subdirs("robot_utils")
+subdirs("robot_maniopulation")
+subdirs("robot_perception")
+subdirs("robot_navigation")
+subdirs("robot_simulation")
---
+++ jetson/robot_ws/build/robot_perception/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/math/Work_2025/jetson/robot_ws/src/robot_perception
+# Build directory: /home/math/Work_2025/jetson/robot_ws/build/robot_perception
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
