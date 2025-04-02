6c1cbff86623509e72bc51f15ef2dcb0abdeb385
MatheusPaivaa <matheuzu11@gmail.com>
2025-04-02 15:08:03 -0300

    removing devel/build

--- jetson/robot_ws/build/robot_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/math/Work_2025/jetson/robot_ws/src/robot_bringup
-# Build directory: /home/math/Work_2025/jetson/robot_ws/build/robot_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- jetson/robot_ws/build/robot_navigation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/math/Work_2025/jetson/robot_ws/src/robot_navigation
-# Build directory: /home/math/Work_2025/jetson/robot_ws/build/robot_navigation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
